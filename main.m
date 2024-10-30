d886d24e7933ce84b9e59ae8bc137d3d4b49b42d
Roberto Bertelli <obe@debian.station>
2024-10-30 10:10:29 +0100

    Code refactoring
    Signed-off-by: Roberto Bertelli <obe@debian.station>

--- main.m
+++ main.m
@@ -4,100 +4,117 @@ addpath('include');
 % 1.1 Angle-axis to rot
 
 disp("#### EXERCISE 1 ####")
-disp("Q1.2");
-disp(AngleAxisToRot([1,0,0], deg2rad(90)));
 
-disp("Q1.3");
-disp(AngleAxisToRot([0,0,1], pi/3));
-
-disp("Q1.4")
+%converting rho to h theta for Q1.4
 rho = [-pi/3, -pi/6, pi/3];
 theta = norm(rho);
 h = rho / theta;
-disp(AngleAxisToRot(h, theta));
+
+ex1_inputs = { 
+    {"Q1.2", [1,0,0], deg2rad(90)}
+    {"Q1.3", [0,0,1], pi/3}
+    {"Q1.4", h, theta}
+};
+
+for i = 1:size(ex1_inputs, 1)
+    input = ex1_inputs{i};
+    exercise = input{1};
+    h = input{2};
+    theta = input{3};
+    R = AngleAxisToRot(h, theta);
+
+    disp("EXERCISE " + exercise);
+    disp("h: ");
+    disp(h);
+    disp("theta: ");
+    disp(theta);
+    disp("Rotation matrix: ");
+    disp(R);
+end
 
 %% 1.2 Rot to angle-axis
 disp("#### EXERCISE 2 ####");
-R1 = [1 0 0; 0 0 -1; 0 1 0];
 
-[h1, theta1] = RotToAngleAxis(R1);
-disp("Q2.2");
-disp("h:");
-disp(h1);
-disp("theta");
-disp(theta1)
+ex2_inputs = {
+    {"Q2.2", [1 0 0; 0 0 -1; 0 1 0]}
+    {"Q2.3", [1 -sqrt(3)/2 0; sqrt(3)/2 0.5 0; 0 0 1]}
+    {"Q2.4", [1 0 0; 0 1 0; 0 0 1]}
+    {"Q2.5", [-1 0 0; 0 -1 0; 0 0 1]}
+};
 
+for i = 1:size(ex2_inputs, 1)
+    input = ex2_inputs{i};
+    exercise = input{1};
+    R = input{2};
+    disp("EXERCISE " + exercise);
+    disp("R:")
+    disp(R)
 
-% R2 = [1 -sqrt(3)/2 0; sqrt(3)/2 0.5 0; 0 0 1];
-% [h2, theta2] = RotToAngleAxis(R2);
-% disp("Q2.3")
-% disp("h:");
-% disp(h2);
-% disp("theta");
-% disp(theta2)
-
-R3 = [1 0 0; 0 1 0; 0 0 1];
-[h3, theta3] = RotToAngleAxis(R3);
-disp("Q2.4")
-disp("h:");
-disp(h3);
-disp("theta");
-disp(theta3)
-
-R4 = [-1 0 0; 0 -1 0; 0 0 1];
-[h4, theta4] = RotToAngleAxis(R4);
-disp("Q2.5")
-disp("h:");
-disp(h4);
-disp("theta");
-disp(theta4)
-
+    try
+        [h, theta] = RotToAngleAxis(R);
+    catch ME
+        disp(ME.message)
+        continue;
+    end
+    
+    disp("h: ")
+    disp(h)
+    disp("theta: ")
+    disp(theta)
+end
 
 %% 1.3 Euler to rot
 disp("#### EXERCISE 3 ####");
 
-Rrpy1 = YPRToRot(0, 0, pi/2);
-disp("Q3.2")
-display(Rrpy1)
+ex3_inputs = {
+   {"Q3.2", 0, 0, pi/2}
+   {"Q3.3", deg2rad(60), 0, 0}
+   {"Q3.4", pi/3, pi/2, pi/4}
+   {"Q3.5", 0, pi/2, -pi/12}
+};
 
-Rrpy2 = YPRToRot(deg2rad(60), 0, 0);
-disp("Q3.3")
-display(Rrpy2)
+for i = 1:size(ex3_inputs, 1)
+    input = ex3_inputs{i};
+    exercise = input{1};
+    psi = input{2};
+    theta = input{3};
+    phi = input{4};
+    R = YPRToRot(psi, theta, phi);
 
-Rrpy3 = YPRToRot(pi/3, pi/2, pi/4);
-disp("Q3.4")
-display(Rrpy3)
-
-Rrpy4 = YPRToRot(0, pi/2, -pi/12);
-disp("Q3.5")
-display(Rrpy4)
-
+    disp("EXERCISE " + exercise);
+    disp("psi:")
+    disp(psi)
+    disp("theta:")
+    disp(theta)
+    disp("phi:")
+    disp(phi)
+    disp("Rotation matrix: ")
+    disp(R)
+end
 
 %% 1.4 Rot to Euler
+ex4_inputs = {
+   {"Q4.2", [1 0 0; 0 0 -1; 0 1 0]}
+   {"Q4.3", [1/2 -sqrt(3)/2 0; sqrt(3)/2 1/2 0; 0 0 1]}
+   {"Q4.4", [0 -sqrt(2)/2 sqrt(2)/2; 0.5 sqrt(2)/4 sqrt(2)/4; -sqrt(3)/2 sqrt(2*sqrt(3))/4 sqrt(2*sqrt(3))/4] }
+};
 
-Rrot1 = [
-    1 0 0;
-    0 0 -1;
-    0 1 0
-];
+for i = 1:size(ex4_inputs, 1)
+    input = ex4_inputs{i};
+    exercise = input{1};
+    R = input{2};
 
-disp("Q4.2")
-display(RotToYPR(Rrot1));
+    [psi, theta, phi] = RotToYPR(R);
 
-Rrot2 = [
-    1/2 -sqrt(3)/2 0;
-    sqrt(3)/2 1/2 0;
-    0 0 1
-];
+    disp("EXERCISE " + exercise);
+    disp("R: ")
+    disp(R)
+    
+    disp("psi:")
+    disp(psi)
+    disp("theta:")
+    disp(theta)
+    disp("phi:")
+    disp(phi)
 
-disp("Q4.3")
-display(RotToYPR(Rrot2));
-
-
-Rrot3 = [
-    0 -sqrt(2)/2 sqrt(2)/2;
-    0.5 sqrt(2)/4 sqrt(2)/4;
-    -sqrt(3)/2 sqrt(2*sqrt(3))/4 sqrt(2*sqrt(3))/4
-];
-disp("Q4.4")
-display(RotToYPR(Rrot3));
+end
