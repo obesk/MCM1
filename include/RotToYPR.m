e3cf391b29e47241d68cef5bf2fc55167f28cf4d
Roberto Bertelli <obe@debian.station>
2024-10-31 18:29:23 +0100

    Sugar code
    Signed-off-by: Roberto Bertelli <obe@debian.station>

--- include/RotToYPR.m
+++ include/RotToYPR.m
@@ -1,18 +1,17 @@
-
 function [psi,theta,phi] = RotToYPR(R)
-
- % Check matrix R to see if its size is 3x3
- tolerance = 10e-10;
-
-if (norm(inv(R) - transpose(R)) > tolerance || abs(det(R) - 1)  > tolerance) 
-    error("The matrix is not a rotation matrix");
+    tolerance = 10e-10;
+    
+    if (norm(inv(R) - transpose(R)) > tolerance || abs(det(R) - 1)  > tolerance) 
+        error("The matrix is not a rotation matrix");
+    end
+    
+    theta = atan2(-R(3,1), sqrt(R(1,1)^2 + R(2,1)^2));
+    if cos(theta) ~= 0
+        psi = atan2(R(2, 1), R(1, 1));
+        phi = atan2(R(3, 2), R(3, 3));
+    else
+        error("This configuration could be a singularity");
+    end
 end
 
-theta = atan2(-R(3,1), sqrt(R(1,1)^2 + R(2,1)^2));
-if cos(theta) ~= 0
-    psi = atan2(R(2, 1), R(1, 1));
-    phi = atan2(R(3, 2), R(3, 3));
-else
-    error("This configuration could be a singularity");
-end
 
